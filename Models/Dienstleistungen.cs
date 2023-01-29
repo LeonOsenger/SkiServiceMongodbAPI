426bb59888c61821640e2c331dc1767f2fb9256e
Leon Osenger <leon@caron.ch>
2023-01-29 18:58:38 +0100

    Models für collection erstellt

---
+++ Models/Dienstleistungen.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace SkiServiceMongodbAPI.Models
+{
+    public class Dienstleistungen
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public int Id { get; set; }
+
+        public string Dienstleistung { get; set; }
+
+        public string Prioritaet { get; set; }
+
+        public string Email { get; set; }
+
+        public string Telefon { get; set; }
+
+        public string Status { get; set; }
+
+        public string Erstell_Datum { get; set; }
+
+        public string Abhol_Datum { get; set; }
+    }
+}
---
+++ Models/Mitarbeiter.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace SkiServiceMongodbAPI.Models
+{
+    public class Mitarbeiter
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Vorname { get; set; }
+
+        public string Passwort { get; set; }
+    }
+}
